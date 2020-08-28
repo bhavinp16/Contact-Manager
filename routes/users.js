8d4b7c85235ad3c5a885ea2d01f458516ae32266
Bhavin <bhavin1625p@gmail.com>
2020-08-28 21:13:52 +0530

    responding with jwt
    jsonwebtoken used to generate and respond with the id for unique users which will act as private key to login to their contacts

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const router = express.Router();
 
-const { check, validationResult } = require('express-validator/check');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
 
@@ -13,7 +15,7 @@ const User = require('../models/User');
 router.post(
 	'/',
 	[
-		check('name', 'Please add name').not.isEmpty(),
+		check('name', 'Please add name').not().isEmpty(),
 		check('email', 'Please include a valid email').isEmail(),
 		check(
 			'password',
@@ -41,13 +43,30 @@ router.post(
 				password,
 			});
 
+			//hashing the password using bcrypt
 			const salt = await bcrypt.genSalt(10);
 
 			user.password = await bcrypt.hash(password, salt);
 
 			await user.save();
 
-			res.send('User saved!');
+			//jwt
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
+			jwt.sign(
+				payload,
+				config.get('jwtSecret'),
+				{
+					expiresIn: 360000,
+				},
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
 		} catch {
 			console.error(err.message);
 			res.status(500).send('Server Error!');
